f51882f2ee4ce384b0bf1c3fd4e1d7c30234ecc2
Luiz Guilherme <luizsilvags07@gmail.com>
2025-09-03 23:42:07 -0300

    Atualizações

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import Botao from './Botao';
+import { useState } from 'react';
 
 const itemsList = [
   {id: 1, name: "Banana", price: 30.46},
@@ -13,19 +14,41 @@ function TabelaItems() {
   return(
     <table>
       <tr>
-        <th>ID</th>
         <th>Nome</th>
         <th>Preco</th>
+        <th>Ações</th>
       </tr>
       {itemsList.map(item => 
         <tr>
-          <td>{item.id}</td>
           <td>{item.name}</td>
           <td>{item.price}</td>
+          <td><Botao nome="Detalhe"/></td>
         </tr>
       )}
     </table>
   );
+}
+
+function FormularioNovoItem() {
+   const [inputs, setInputs] = useState({});
+
+  const handleChange = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setInputs(values => ({...values, [name]: value}))
+  }
+  return (
+    <form>
+      <label>Nome:
+        <input type="text" name="nome" value={inputs.nome} onChange={handleChange}/>
+      </label>
+      <br />
+      <label>Preco:
+        <input type="text" name="preco" value={inputs.preco} onChange={handleChange} />
+      </label>
+      <input type="submit" />
+    </form>
+  )
 }
 
 function GridItems() {
@@ -38,5 +61,8 @@ function GridItems() {
 }
 
 createRoot(document.getElementById('root')).render(
+  <>
   <GridItems/>
+  <FormularioNovoItem/>
+  </>
 )
